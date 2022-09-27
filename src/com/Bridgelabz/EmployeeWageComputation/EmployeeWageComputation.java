ec7aaebe9dd8adce2ef5630c66f3c464ace1c9ff
vishalshinde541 <vishalshinde541@gmail.com>
2022-09-28 00:44:16 +0530

    Initial commit 1

---
+++ src/com/Bridgelabz/EmployeeWageComputation/EmployeeWageComputation.java
@@ -0,0 +1,9 @@
+package com.Bridgelabz.EmployeeWageComputation;
+
+public class EmployeeWageComputation {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		
+	}
+
+}
